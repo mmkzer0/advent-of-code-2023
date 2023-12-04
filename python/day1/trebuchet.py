55115aa5fec5cfb360d4518760bbf21d8e258108
Lennart Kotzur <lennart@Lennarts-MacBook-Pro.local>
2023-12-04 22:42:45 +0100

    Second harder puzzle solved; most cursed replace of all time because I am incredibly lazy, will do proper regex later

--- python/day1/trebuchet.py
+++ python/day1/trebuchet.py
@@ -2,14 +2,18 @@ import sys
 import re
 
 input = sys.stdin
+replacements = {"one" : "o1e", "two" : "t2o", "three" : "thr3e", "four" : "fo4r", "five" : "fi5e", "six" : "s6x", "seven" : "se7en", "eight" : "ei8ht", "nine" : "ni9e"}
 
 def findDigitSum(x):
     sum = int()
 
     for line in x:
+        for old, new in replacements.items():
+            line = line.replace(old, new)
+
         line = re.findall(r'\d+', line)
         line = ''.join(line)
-        
+
         if len(line) == 1:
             sum += int(line[0]*2)
         else:
